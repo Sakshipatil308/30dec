29cd6106963ef5f8decda4a75fbec8d4aa843ae2
Sakshipatil308 <93004059+Sakshipatil308@users.noreply.github.com>
2022-01-02 19:39:01 +0530

    Add files via upload

---
+++ desending.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class desending {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int a[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int ind = s.charAt(i) - 'a';
+            a[ind]++;
+        }
+        for (int i = a.length - 1; i >= 0; i--) {
+            for (int j = 0; j < a[i]; j++) {
+                System.out.print((char) (i + 'a'));
+            }
+        }
+
+    }
+
+}
